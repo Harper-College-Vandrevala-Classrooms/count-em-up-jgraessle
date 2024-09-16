0b76057a1f26256df3a03def3985d1ebd929df14
jgraessle <j_graessle@mail.harpercollege.edu>
2024-09-15 18:02:16 -0700

    New GroceryCounter program and Tests
    A GroceryCounter program was added that adds numbers up like a grocery counter. Tests were implemented to ensure each digit of the counter worked as expected. Each digit of the counter is pressed as many times as the user inputs and resets every time it reaches 10 and adds one to the next digit. Ones the tens digit reaches 10, one is added to the overflow counter and the tens digit resets to 0.

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,109 @@
 package com.csc;
 
+import java.util.Scanner;
+
 public class GroceryCounter {
-  // Put your code here!
+
+  // class variables
+  int hundreths;
+  int tenths;
+  int ones;
+  int tens;
+  int overflows;
+
+  // default class definition with all variables = 0
+  public GroceryCounter() {
+    hundreths = 0;
+    tenths = 0;
+    ones = 0;
+    tens = 0;
+  }
+
+  // hundreths function
+  public int hundreths(int hundreths) {
+    while (hundreths >= 10) {
+      hundreths = hundreths - 10;
+      tenths = tenths + 1;
+    }
+    return hundreths;
+  }
+
+  // tenths function
+  public int tenths(int tenths) {
+    while (tenths >= 10) {
+      tenths = tenths - 10;
+      ones = ones + 1;
+    }
+    return tenths;
+  }
+
+  // ones function
+  public int ones(int ones) {
+    while (ones >= 10) {
+      ones = ones - 10;
+      tens = tens + 1;
+    }
+    return ones;
+  }
+
+  // tens function
+  public int tens(int tens) {
+    while (tens >= 10) {
+      tens = tens - 10;
+      overflows = overflows + 1;
+    }
+    return tens;
+  }
+
+  // total function (compiles and formats previous results)
+  public String total(int hundreths, int tenths, int ones, int tens) {
+    String total;
+    hundreths = hundreths(this.hundreths);
+    tenths = tenths(this.tenths);
+    ones = ones(this.ones);
+    tens = tens(this.tens);
+    if (tens == 0) {
+      total = "$" + ones + "." + tenths + hundreths;
+    }
+    else {
+      total = "$" + tens + ones + "." + tenths + hundreths;
+    }
+    return total;
+  }
+
+  // overflows returner
+  public int number_of_overflows() {
+    return overflows;
+  }
+
+  // clearing function
+  public void clear() {
+    hundreths = 0;
+    tenths = 0;
+    ones = 0;
+    tens = 0;
+    overflows = 0;
+  }
+public static void main(String[] args) {
+
+  // definiting objects
+  Scanner scanner = new Scanner(System.in);
+  GroceryCounter counter = new GroceryCounter();
+
+  // prompting
+  System.out.print("Enter number of hundreths place taps: ");
+  counter.hundreths = scanner.nextInt();
+  System.out.print("Enter number of tenths place taps: ");
+  counter.tenths = scanner.nextInt();
+  System.out.print("Enter number of ones place taps: ");
+  counter.ones = scanner.nextInt();
+  System.out.print("Enter number of tens place taps: ");
+  counter.tens = scanner.nextInt();
+  scanner.close();
+
+  // final result
+  System.out.println(counter.total(counter.hundreths, counter.tenths, counter.ones, counter.tens));
+  System.out.println("overflows: " + counter.number_of_overflows());
+
+} 
 }
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -15,10 +15,53 @@ public class TestGroceryCounter {
   }
 
   // Put your tests here!
-
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void ZeroHundrethsWorks() {
+    assertEquals(0, counter.hundreths(0));
   }
-
+  @Test
+  void FiveHundrethsWorks() {
+    assertEquals(5, counter.hundreths(5));
+  }
+  @Test
+  void TenHundrethsWorks() {
+    assertEquals(0, counter.hundreths(10));
+  }
+  @Test
+  void ZeroTenthsWorks() {
+    assertEquals(0, counter.tenths(0));
+  }
+  @Test
+  void FiveTenthsWorks() {
+    assertEquals(5, counter.tenths(5));
+  }
+  @Test
+  void TenTenthsWorks() {
+    assertEquals(0, counter.tenths(10));
+  } 
+  @Test
+  void ZeroOnesWorks() {
+    assertEquals(0, counter.ones(0));
+  }
+  @Test
+  void FiveOnesWorks() {
+    assertEquals(5, counter.ones(5));
+  }
+  @Test
+  void TenOnesWorks() {
+    assertEquals(0, counter.ones(10));
+  } 
+  @Test
+  void ZeroTensWorks() {
+    assertEquals(0, counter.tens(0));
+  }
+  @Test
+  void FiveTensWorks() {
+    assertEquals(5, counter.tens(5));
+  }
+  @Test
+  void TenTensWorks() {
+    assertEquals(0, counter.tens(10));
+    assertEquals(1, counter.number_of_overflows());
+  }
 }
